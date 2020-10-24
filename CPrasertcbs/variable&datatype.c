13a7b4526d4e80a30ba80b391959b2015ed746a5
Chawintee <chawinteetea@gmail.com>
2020-10-24 22:37:38 +0700

    learn type of variable

---
+++ CPrasertcbs/variable&datatype.c
@@ -0,0 +1,31 @@
+//
+// Created by lenovoY410P on 10/24/2020.
+//
+
+//function + variable
+#include <stdio.h>
+int main(){
+    printf("hello world!!\n"); // \n -> new line
+    printf("Don't worry be happy.\n");
+    int age; //integer
+    int Age, aGE, AGE;
+    double price;
+    age = 28;
+    Age = 99; // case sensitive
+    price = 899.75;
+    int score = 70;
+    price = price * 1.1;
+    printf("%d\n", age);
+    printf("%f\n", price);
+    printf("%.2f\n", price);
+
+    char *hero = "spiderman";
+    printf("%s\n", hero);
+    char country[] = "Thailand";
+    printf("%s\n", country);
+
+
+
+    return 0;
+}
+
