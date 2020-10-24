ef9330f82047cacbf7dc6cf8c8f8e5c9a891787c
Chawintee <chawinteetea@gmail.com>
2020-10-24 22:34:23 +0700

    add calculator learn operation in c

---
+++ CPrasertcbs/Calculator.c
@@ -0,0 +1,36 @@
+//
+// Created by lenovoY410P on 10/24/2020.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+//    printf("%d\n",5 + 3);
+//    printf("%d\n",5 - 3);
+//    printf("%d\n",5 * 3);
+//    printf("%d\n",5 / 3);
+//    printf("%d\n",14 / 3);
+
+//    printf("%d\n",5.0 / 3.0);
+//    printf("%f\n",5.0 / 3.0);
+//    printf("%f\n",5.0 / 3);
+//    printf("%f\n",5 / 3.0);
+
+    // int vs double/float
+    printf("%f\n", 5 + 3 * 2 - 4);
+    printf("%f\n", 5.0 + 3 * 2 - 4);
+    printf("%f\n", 5.0 + (3 * 2) - 4);
+    printf("%f\n", ((5.0 + 3) * 2) - 4);
+    printf("%f\n", -5.0 + 6 - 4 / 2 - 1 * 9);
+    printf("%f\n", -5.0 + 6 - (4 / 2) - (1 * 9));
+    printf("%f\n",pow(3,2));
+    printf("%d\n",pow(3,2));
+
+
+
+
+
+
+    return 0;
+}
