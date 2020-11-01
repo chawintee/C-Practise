a022c2f66cc81e27393a3bd50766ae63d66cb5dd
Chawintee <chawinteetea@gmail.com>
2020-11-01 22:43:33 +0700

    function declaration

---
+++ CPrasertcbs/declaration.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+//function declaration / prototype
+double triangle(double b, double h);
+
+double circle( double r){
+    return 3.14 * r *r ;
+}
+
+ double rectangle(double w, double h){
+     return w * h;
+ }
+
+//in c have to declare function before use in main;
+ int main(){
+     printf("rectangle area = %.2f \n", rectangle(3,5));
+     printf("circle area = %.2f \n", circle(3));
+     printf("triangle area = %.2f \n", triangle(3,5));
+     return 0;
+ }
+
+double triangle(double b, double h){
+    return .5 * b * h;
+}
+
